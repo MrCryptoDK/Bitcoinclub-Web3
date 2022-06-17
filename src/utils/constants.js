3d4a6a37ebb601ab9d23092d605f9cc75b22559c
MrCryptoDK <kevin14dk@gmail.com>
2022-06-17 00:52:05 -0500

    contrato desplegado

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xB82d4564b2e2BaB4952EcE39bB383dE7590cF03B"
+export const contractAddress = "0xFd421294A88bAb80E49cb2A5857a654330E8afFe" // anterior "0xB82d4564b2e2BaB4952EcE39bB383dE7590cF03B"
 export const contractABI = abi.abi;
 
 
